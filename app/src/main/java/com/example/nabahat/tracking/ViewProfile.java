745f913577683b5b807d34d71132006ba2f0c4e6
Nabahats123 <nabahats123@gmail.com>
2018-03-28 21:00:44 +0500

    Updates

--- app/src/main/java/com/example/nabahat/tracking/ViewProfile.java
+++ app/src/main/java/com/example/nabahat/tracking/ViewProfile.java
@@ -48,7 +48,7 @@ public class ViewProfile extends AppCompatActivity {
             public void onDataChange(DataSnapshot dataSnapshot) {
                 Log.i("Data Received",dataSnapshot.toString());
                 if (dataSnapshot.exists()) {
-                    for (DataSnapshot ds : dataSnapshot.getChildren()) {
+
                         String name= dataSnapshot.child("username").getValue(String.class);
                         String email= dataSnapshot.child("email").getValue(String.class);
                         String phone= dataSnapshot.child("phonenumber").getValue(String.class);
@@ -57,7 +57,24 @@ public class ViewProfile extends AppCompatActivity {
                         displayemail.setText(email);
                         displaynumber.setText(phone);
                         displayroute.setText("Route Number: "+route);
-                    }
+
+                        int ratingsum = 0;
+                        int ratingtotal = 0;
+                        float ratingsaverage = 0;
+                        for (DataSnapshot child: dataSnapshot.child("ratings").getChildren()){
+                            ratingsum = ratingsum + Integer.valueOf(child.getValue().toString());
+                            ratingtotal++;
+
+                        }
+                        if(ratingtotal!=0){
+                            ratingsaverage = ratingsum/ratingtotal;
+                            ratingbar.setRating(ratingsaverage);
+                            String avg = Float.toString(ratingsaverage);
+
+                            rating.setText(avg+"("+ratingtotal+")");
+                            //Toast.makeText(ViewProfile.this, avg+" ("+ratingtotal+") " , Toast.LENGTH_SHORT).show();
+                        }
+
 
                 }
                 else{
